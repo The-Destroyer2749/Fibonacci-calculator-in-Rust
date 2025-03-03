0899291abe3c89be9571b50325eda0380c49f526
Philip Hoderny <pokemonphilip8647@gmail.com>
2025-03-02 21:32:26 -0500

    Fixed capitalization errors Removed unnecessary code Added quality of life features Removed a stack overflow bug that was caused by the recursive Fibonacci solver by replacing it with an iterative solver Improved performance by letting the user control how often the results are returned during the iterative mode Added basic comments Removed unnecessary control flow when in program 1 Tidied up code Fixed formatting errors both in my code and in the output

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,66 @@
+// imports
 use text_io::*;
 use num::BigUint;
 
 fn main() {
-    let mut useless_string: String;
-    let mut input1: i128;
-    let mut input2: String;
-    let mut iteration_goal: i128;
-    let mut result: (BigUint, BigUint);
+    // const
     let zero_as_vec: Vec<u32> = vec![0];
     let one_as_vec: Vec<u32> = vec![1];
     let big_uint_0: BigUint = BigUint::new(zero_as_vec);
     let big_uint_1: BigUint = BigUint::new(one_as_vec);
+    
+    // mutable
+    let mut running: bool = true;
+    let mut input1: i128;
+    let mut iteration_goal: i128;
+    let mut result: (BigUint, BigUint);
     let mut num1: BigUint = big_uint_0;
     let mut num2: BigUint = big_uint_1;
-    let mut running: bool = true;
+    let mut print_step_size: i128;
     
     println!("Welcome!\n\n");
-    while running == true {
-        println!("Input 1 to ask for specific fibonacci numbers \ninput 2 for a iterative output up to a certain number \n input 3 to exit the program");
+    
+    while running == true { // program loop
+        println!("Input 1 to ask for specific Fibonacci numbers \nInput 2 for a iterative output up to a certain number \nInput 3 to exit the program");
         let program: i8 = read!();
-
-        if program == 1 {
-            loop {
-                println!("What fibonacci number do you want?");
-                input1 = read!();
-                iteration_goal = input1;
-                result = fib(num1.clone(), num2.clone(), 0, iteration_goal);
-                println!("The fibonacci sequence at the index: {} is {}", iteration_goal, result.0);
-                println!("\nWould you like to continue?");
-                input2 = read!();
-                input2 = input2.to_lowercase();
-                if input2 != "true" && input2 != "yes" && input2 != "y" {
-                    break;
-                }
-            }
-        } else if program == 2 {
-            println!("what number do you want to iterate up to?");
+        
+        if program == 1 { // direct Fibonacci number program
+            println!("What fibonacci number do you want?");
+            input1 = read!();
+            iteration_goal = input1;
+            result = fib(num1.clone(), num2.clone(), 0, iteration_goal);
+            println!("The Fibonacci sequence at the index {} is: {}\n", iteration_goal, result.0);
+        }
+        else if program == 2 { // iterative Fibonacci number program
+            println!("What number do you want to iterate up to?");
             iteration_goal = read!();
+            println!("How many iterations should it preform before printing each value?");
+            print_step_size = read!();
             for i in 0..iteration_goal + 1 {
                 result = (num2.clone(), num1 + num2);
                 num1 = result.0;
                 num2 = result.1;
-                println!("The result at iteration: {} is {}", i, num2);
+                if i % print_step_size == 0 || i == iteration_goal {
+                    println!("The result at iteration {} is: {}\n", i, num2);
+                }
             }
         }
-        else if program == 3 { 
+        else if program == 3 { // program exit
             running = false;
         }
         else { 
-            println!("Error: invalid input");
+            println!("Error: invalid input\n");
         }
     }
-    useless_string = "".to_string();
-    println!("This is just to hold off the program :) type anything to end it {}", useless_string);
-    useless_string = read!();
-    println!("{}", useless_string);
 }
 
-
-fn fib(num1: BigUint, num2: BigUint, mut i: i128, iteration_goal: i128) -> (BigUint,BigUint) {
-    i+=1;
-    if i == iteration_goal {
-        (num1, num2)
+fn fib(mut num1: BigUint, mut num2: BigUint, mut i: i128, iteration_goal: i128) -> (BigUint,BigUint) { // iterative Fibonacci solver
+    let mut temp: BigUint;
+    while i != iteration_goal {
+        i+=1;
+        temp = num1;
+        num1 = num2.clone();
+        num2 = num2 + temp;
     }
-    else {
-        fib(num2.clone(), num1 + num2, i, iteration_goal)
-    }
+    (num1, num2)
 }
